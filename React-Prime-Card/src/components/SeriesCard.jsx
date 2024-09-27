21d93de4e7666e33491c28c07be1b1e6946682bd
Zero-7-1 <109064402+Zero-7-1@users.noreply.github.com>
2024-09-27 12:07:31 +0530

    handled props better

--- React-Prime-Card/src/components/SeriesCard.jsx
+++ React-Prime-Card/src/components/SeriesCard.jsx
@@ -1,23 +1,30 @@
 import React from 'react';
 
 
+/*
 
-export const SeriesCard = (props) => {
-    return  (  
+In SeriesCard, destructured series directly in the function parameters so that
+now, instead of using props.series.seriesName, etc. , we can directly use seriesName, 
+seriesRatings, etc.
+*/
+export const SeriesCard = ({ series: 
+         { seriesImage, seriesName, seriesRatings, seriesAbout, seriesGenre }, 
+         canWatch }) => {
+    return (  
         <li>
            <div>
               <img 
-                 src={props.series.seriesImage} 
-                 alt={props.series.seriesName} 
+                 src={seriesImage} 
+                 alt={seriesName} 
                  width="40%" 
                  height="40%" 
               />
            </div>
-           <h1>{props.series.seriesName}</h1>
-           <h3>Ratings: {props.series.seriesRatings}</h3>
-           <p>{props.series.seriesAbout}</p>
-           <p>Genre: {props.series.seriesGenre}</p>
-           <button>{props.canWatch(props.series.seriesRatings)}</button>
+           <h1>{seriesName}</h1>
+           <h3>Ratings: {seriesRatings}</h3>
+           <p>{seriesAbout}</p>
+           <p>Genre: {seriesGenre}</p>
+           <button>{canWatch(seriesRatings)}</button>
         </li>
     ); 
 };
